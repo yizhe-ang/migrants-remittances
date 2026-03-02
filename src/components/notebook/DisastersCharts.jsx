19f4323d47f6279f843c3b3492b78d7645c105e1
yizhe-ang <ang.yizhe@u.nus.edu>
2026-03-02 12:28:09 +0100

    update

--- src/components/notebook/DisastersCharts.jsx
+++ src/components/notebook/DisastersCharts.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useMemo, useRef } from "react";
 import * as Plot from "@observablehq/plot";
 import { useSql } from "@sqlrooms/duckdb";
+import { rollup, sum } from "d3-array";
+
+const disasterTypeMap = new Map([
+  ["Flood", "floods"],
+  ["Earthquake", "earthquakes"],
+  ["Storm", "storms"],
+  ["Drought", "droughts"],
+]);
 
 const DisastersCharts = ({ ...props }) => {
-  const { disasters, disastersImpacts } = props;
+  const { disasters, disastersImpactsByMonth } = props;
 
   const disastersProcessed = useMemo(() => {
     return disasters.toArray().map((d) => {
@@ -15,14 +23,62 @@ const DisastersCharts = ({ ...props }) => {
     });
   }, [disasters]);
 
+  const disastersImpactsByMonthProcessed = useMemo(() => {
+    return disastersImpactsByMonth.toArray().map((d) => {
+      return {
+        ...d,
+        date: new Date(d.date),
+        // Affected: Number(d.Affected),
+      };
+    });
+  }, [disastersImpactsByMonth]);
+
+  const aggData = useMemo(() => {
+    const o = Object.fromEntries(
+      [...disasterTypeMap.keys()].map((v) => [v, {}]),
+    );
+
+    const remittancesByDisaster = rollup(
+      disastersImpactsByMonth,
+      (v) => sum(v, (d) => d.remittance),
+      (d) => d.disaster_type,
+    );
+
+    const affectedByDisaster = rollup(
+      disastersProcessed,
+      (v) => sum(v, (d) => d.Affected),
+      (d) => d["Disaster Type"],
+    );
+
+    [...disasterTypeMap.keys()].forEach((k) => {
+      o[k].remittances = remittancesByDisaster.get(disasterTypeMap.get(k));
+
+      o[k].affected = affectedByDisaster.get(k);
+
+      o[k].remittance_per_affected = o[k].remittances / o[k].affected;
+    });
+
+    return Object.entries(o).map(([k, v]) => {
+      return {
+        ...v,
+        disaster_type: k,
+      };
+    });
+  }, [disastersImpactsByMonth, disastersProcessed]);
+
+  console.log(aggData);
+
   const containerRef1 = useRef();
   const containerRef2 = useRef();
   const containerRef3 = useRef();
   const containerRef4 = useRef();
+  const containerRef5 = useRef();
+  const containerRef6 = useRef();
 
   // Bee swarm
   useEffect(() => {
     const plot = Plot.plot({
+      title: "People affected by each disaster, over time",
       height: 500,
       width: 700,
       color: {
@@ -177,35 +233,71 @@ const DisastersCharts = ({ ...props }) => {
     // });
   }, []);
 
-  // useEffect(() => {
-  //   const plot = Plot.plot({
-  //     height: 500,
-  //     width: 700,
-  //     marginLeft: 80,
-  //     color: {
-  //       // legend: true,
-  //       domain: ["Flood", "Earthquake", "Drought", "Storm"],
-  //     },
-  //     marks: [
-  //       Plot.rectY(disastersProcessed, {
-  //         x: "Date",
-  //         y: "Affected",
-  //         fill: "Disaster Type",
-  //         interval: "year",
-  //       }),
-  //     ],
-  //   });
+  useEffect(() => {
+    const plot = Plot.plot({
+      title: "Remittances induced by each disaster type, over time",
+      height: 300,
+      width: 700,
+      marginLeft: 80,
+      color: {
+        domain: ["floods", "earthquakes", "droughts", "storms"],
+      },
+      marks: [
+        Plot.areaY(disastersImpactsByMonthProcessed, {
+          x: "date",
+          y: "remittance",
+          fill: "disaster_type",
+          // order: "sum",
+          offset: "wiggle",
+        }),
+      ],
+    });
 
-  //   containerRef2.current.append(plot);
+    containerRef5.current.append(plot);
 
-  //   return () => plot.remove();
-  //   // DEBUG:
-  //   });
+    return () => plot.remove();
+    // DEBUG:
+    // });
+  }, []);
+
+  useEffect(() => {
+    const plot = Plot.plot({
+      title:
+        "Total remittances induced by each disaster type, from 2010 to 2019",
+      height: 110,
+      width: 700,
+      marginLeft: 90,
+      color: {
+        domain: ["floods", "earthquakes", "droughts", "storms"],
+      },
+      marks: [
+        Plot.barX(
+          disastersImpactsByMonthProcessed,
+          Plot.groupY(
+            {
+              x: "sum",
+            },
+            {
+              y: "disaster_type",
+              x: "remittance",
+              fill: "disaster_type",
+              sort: { y: "x", reverse: true },
+            },
+          ),
+        ),
+      ],
+    });
+
+    containerRef2.current.append(plot);
+
+    return () => plot.remove();
+    // DEBUG:
+  });
   // }, []);
 
   useEffect(() => {
     const plot = Plot.plot({
-      height: 300,
+      height: 500,
       width: 700,
       marginLeft: 80,
       color: {
@@ -233,7 +325,37 @@ const DisastersCharts = ({ ...props }) => {
       ],
     });
 
-    containerRef3.current.append(plot);
+    containerRef4.current.append(plot);
+
+    return () => plot.remove();
+    // DEBUG:
+    // });
+  }, []);
+
+  useEffect(() => {
+    const plot = Plot.plot({
+      height: 300,
+      width: 700,
+      marginLeft: 80,
+      color: {
+        // legend: true,
+        domain: ["Flood", "Earthquake", "Drought", "Storm"],
+      },
+      marks: [
+        Plot.rectY(
+          aggData,
+          Plot.stackX({
+            x: "affected",
+            y2: "remittance_per_affected",
+            fill: "disaster_type",
+            order: "remittance_per_affected",
+            reverse: true,
+          }),
+        ),
+      ],
+    });
+
+    containerRef6.current.append(plot);
 
     return () => plot.remove();
     // DEBUG:
@@ -252,13 +374,15 @@ const DisastersCharts = ({ ...props }) => {
 
       <div className="mx-auto mt-5" ref={containerRef1} />
 
+      <div className="mx-auto mt-5" ref={containerRef3} />
+
       <div className="mx-auto mt-5" ref={containerRef4} />
+
+      <div className="mx-auto mt-5" ref={containerRef5} />
 
       <div className="mx-auto mt-5" ref={containerRef2} />
 
-      <div className="mx-auto mt-5" ref={containerRef3} />
-
-      {/* <div className="mx-auto mt-5" ref={containerRef3} /> */}
+      <div className="mx-auto mt-5" ref={containerRef6} />
 
       <div className="w-xl mx-auto mt-5 text-sm">
         We estimate that the occurrence of a disaster, on average, increases
