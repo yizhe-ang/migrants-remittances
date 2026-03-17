1d81c096b15f0832f2d3b2dc651c5c3141e45fbe
yizhe-ang <ang.yizhe@u.nus.edu>
2026-03-17 13:39:00 +0100

    update

--- src/store/index.js
+++ src/store/index.js
@@ -118,7 +118,7 @@ export const { roomStore, useRoomStore } = createRoomStore(
     setSankeyIncome: (sankeyIncome) => set({ sankeyIncome }),
 
     // Controls / Interactions #################################################
-    enableMapInteractions: false,
+    enableMapInteractions: true,
     setEnableMapInteractions: (enableInteractions) =>
       set({ enableInteractions }),
 
