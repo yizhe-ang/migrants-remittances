b47c8a2f22501a45255b60c7cb2bb41767121522
yizhe-ang <ang.yizhe@u.nus.edu>
2026-04-08 14:47:13 +0200

    update

--- src/components/interface/Controls.jsx
+++ src/components/interface/Controls.jsx
@@ -58,7 +58,9 @@ const Controls = () => {
           >
             {/* <div className="text-sm pl-1 text-stone-500">size represents</div> */}
             <Toggle
-              className="text-lg"
+              className="text-lg
+              aria-pressed:bg-[repeating-linear-gradient(-45deg,#f5f5f4_0px,#f5f5f4_6px,#d6d3d1_4px,#d6d3d1_8px)]
+              "
               pressed={pointsValue[0] === "propGdp"}
               onPressedChange={(val) => {
                 setPointsValue(val ? ["propGdp"] : ["absolute"]);
