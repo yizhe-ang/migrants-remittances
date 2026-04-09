6949b6c8ed5dcb3d52dad6187aa2bef56c047ed9
yizhe-ang <ang.yizhe@u.nus.edu>
2026-04-09 16:37:23 +0200

    update

--- src/components/interface/About.jsx
+++ src/components/interface/About.jsx
@@ -1,15 +1,33 @@
+import { useEffect } from "react";
+import { useLenis } from "lenis/react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useRoomStore } from "@/store";
 
 const About = () => {
+  const openAbout = useRoomStore((state) => state.openAbout);
+  const setOpenAbout = useRoomStore((state) => state.setOpenAbout);
+  const lenis = useLenis();
+
+  useEffect(() => {
+    if (openAbout) {
+      lenis?.stop();
+    } else {
+      lenis?.start();
+    }
+
+    return () => {
+      lenis?.start();
+    };
+  }, [lenis, openAbout]);
+
   return (
-    <Dialog>
+    <Dialog open={openAbout} onOpenChange={setOpenAbout}>
       <DialogTrigger className="font-bold text-xl cursor-pointer">
         About
       </DialogTrigger>
